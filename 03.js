2ac20453f0710bf2a591d767c60971bf21b6558d
javierc22 <javier.ec.22@gmail.com>
2019-12-24 15:18:58 -0300

    Propiedades computadas

--- 03.js
+++ 03.js
@@ -1,6 +1,19 @@
 const app = new Vue ({
     el: '#app',
     data: {
-        title: 'Vue JS - Ejemplo 3: Propiedades computadas'
+        title: 'Vue JS - Ejemplo 3: Propiedades computadas',
+        contador: 0
+    },
+    computed: {
+        invertido(){
+            return this.title.split('').reverse().join('');
+        },
+        color(){
+            return {
+                'bg-success': this.contador <= 10,
+                'bg-warning': this.contador > 10 && this.contador < 20,
+                'bg-danger': this.contador >= 20
+            }
+        }
     }
 });
